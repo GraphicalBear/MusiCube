4c10ed0dad568b59b3e8ca917e0c2c2f5b5f5c22
mike <mike@mike-Vostro-1015.(none)>
2015-07-13 12:35:16 -0400

    Added a shell script to install all the needed libraries for synthesis. It should work (in theory).

---
+++ get_libraries.sh
@@ -0,0 +1,1 @@
+apt-get install libportmidi-dev portaudio19-dev liblo-dev libsndfile-dev python-dev python-tk subversion python-imaging-tk python-wxgtk2.8 && svn checkout http://pyo.googlecode.com/svn/trunk/ pyo-read-only && cd pyo-read-only && python setup.py install --install-layout-deb
