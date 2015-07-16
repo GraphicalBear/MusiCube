1014bbf5d5e792d96c415e55e6c6c8023ef02574
mike <mike@mike-Vostro-1015.(none)>
2015-07-16 08:59:05 -0400

    Changed the detection code to use absolute value

--- Python/alsa_sound_control.py
+++ Python/alsa_sound_control.py
@@ -5,7 +5,6 @@ from pygame import mixer
 
 mixer.init()
 
-
 running = True
 elapsed_time = 0
 next_value = 25
@@ -39,18 +38,10 @@ elapsed_time = time.time()
 while running:
     if serialPort.inWaiting() > 0:
         next_value = serialPort.readline()
-    if next_value < 20 or next_value > 30:
+    if abs(next_value) > 1000:
         elapsed_time = time.time()
         serialPort.write(bytes([1]))
-    if next_value >= 20 and next_value <= 30:
+    else:
         if elapsed_time - time.time() >= 25:
             play_song()
             serialPort.write(bytes([0]))
-
-# os.startfile('filepath')
-#
-# if y_axis > 130:
-#     os.system('amixer set Master 5+')
-# if y_axis > 100 and y_axis < 120:
-#     os.system('amixer set Master 5-')
-# if x_axis > 20 and x_axis < 30 and y_axis > 120 and y_axis < 130 and z_axis > 220 and z_axis < 230:
